fef930063b5c4184c32f7da5742b1cd5a68b56c3
Rodrigo Peretta da Silva <rperetta@img.com.br>
2017-03-06 14:04:13 -0300

    first commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAN_11QZ7GdrU1oZFUd1iEtgtnAdcpEyXI",
+    authDomain: "biblioteca-86a53.firebaseapp.com",
+    databaseURL: "https://biblioteca-86a53.firebaseio.com",
+    storageBucket: "biblioteca-86a53.appspot.com",
+    messagingSenderId: "84358734465"
+}
